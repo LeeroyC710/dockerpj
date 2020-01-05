9f772e03f279b7f7608238e68462141460059cf6
LeeroyC710 <57941669+LeeroyC710@users.noreply.github.com>
2020-01-05 22:16:33 +0000

    Update app.py

--- bignum/app.py
+++ bignum/app.py
@@ -2,18 +2,19 @@ from flask import Flask, jsonify, make_response
 import sys
 import requests
 import randint
+import random 
 import string
 app = Flask(__name__)
 
 #---------------prize generator function------------------------------
 @app.route('/prize_gen_big', methods=['GET', 'POST'])
 def prize_gen_big():
-    chance = randint(0,100)
+    chance = random.randint(0,100)
     prize = 0
     make_response = "You didn't win a prize"
     
     if chance >= 70:
-        prize = randint(50,100)
+        prize = random.randint(50,100)
         resp = requests.get('http://notification:9000/notify').content    
 
     payload = request.get_json(force = True)
