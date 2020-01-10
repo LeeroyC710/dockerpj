ba1689289f790046a55773c688547b67ea37febb
Leeroy Chiweshe <Leeroy@Leeroys-MBP.lan>
2020-01-10 00:31:36 +0000

    changed routes.py services

--- service3/application/routes.py
+++ service3/application/routes.py
@@ -13,31 +13,31 @@ def dare_generator():
         random_letter = requests.post('http://127.0.0.1:5002').json()
         data['number'] = random_number['number']
         data['letter'] = random_letter['letter']
-
-        return data 
+        print(data, "0")
+        #return data 
     #random_number['number']
 
-        if data['number']  == '0' and data['letter'] == 'A': 
+        if data['number']  == 0 and data['letter'] == 'a': 
             return {"d0":"Number: 0, Letter: A, Dare: You have to avoid calling people by names for 5mins"} 
-        elif data['number'] =='0' and data['letter'] == 'B': 
+        elif data['number'] == 0 and data['letter'] == 'b': 
             return {"d0":"Number: 0, Letter: B, Dare: Tell the next person that you have never ever met them"}
-        elif data['number'] =='0' and data['letter'] == 'C': 
+        elif data['number'] == 0 and data['letter'] == 'c': 
             return {"d0":"Number: 0, Letter: C, Dare: Change the song playing to a classical song of you choice"}
-        elif data['number'] =='0' and data['letter'] == 'D':
+        elif data['number'] == 0 and data['letter'] == 'd':
             return {"d0":"Number: 0, Letter: D, Dare: 100push-ups in 5mins"}
-        elif data['number'] =='1' and data['letter'] == 'A': 
+        elif data['number'] == 1 and data['letter'] == 'a': 
             return {"d0":"Number: 1, Letter: A, Dare: Tell everyone your most embarrassing moment"} 
-        elif data['number'] =='1' and data['letter'] == 'B': 
+        elif data['number'] == 1 and data['letter'] == 'b': 
             return {"d0":"Number: 1, Letter: B, Dare: Eat a teaspoon of mustard"} 
-        elif data['number'] =='1' and data['letter'] == 'C': 
+        elif data['number'] == 1 and data['letter'] == 'c': 
             return {"d0":"Number: 1, Letter: C, Dare: Dare someone of your choice"} 
-        elif data['number'] =='1' and data['letter'] == 'D':
+        elif data['number'] == 1 and data['letter'] == 'd':
             return {"d0":"Number: 1, Letter: D, Dare: Do 20 press-ups without stoping in 30secs"}
-        elif data['number'] =='2' and data['letter'] == 'A': 
+        elif data['number'] == 2 and data['letter'] == 'a': 
             return {"d0":"Number: 2, Letter: A, Dare: Scream bogies very loud, right now!"} 
-        elif data['number'] =='2' and data['letter'] == 'B': 
+        elif data['number'] == 2 and data['letter'] == 'b': 
             return {"d0":"Number: 2, Letter: B, Dare: Shout i love myself"} 
-        elif data['number'] =='2' and data['letter'] == 'C': 
+        elif data['number'] == 2 and data['letter'] == 'd': 
             return {"d0":"Number: 2, Letter: C, Dare: Tell your friends you kissed Mariah Carey"}
         else:  
             return {"d0":"Number: 2, Letter: D, Dare: Have an arm-wrestling with the person sitting next to you!"}
