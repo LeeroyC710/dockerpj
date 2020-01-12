1976c27278f845fc89e64ddf3f0fb6f895381e05
leeroyc710 <leeroy.chiweshe@gmail.com>
2020-01-12 16:23:07 +0000

    secret key sorted

--- service4/application/__init__.py
+++ service4/application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app=Flask(__name__)
-app.config['SECRET_KET'] = 'yes'
+app.config['SECRET_KEY'] = 'yes'
 #--------------------code for modelling databases check models.py-------------------------------------
 
 from application import routes
